ba7722700a709930c0f211ca01d415255c9d81cd
unknown <marcinoles87@gmail.com>
2022-11-21 17:18:14 +0100

    add wheel and function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@
 import './App.css';
 
 function App() {
+
+  const radius = Math.floor(Math.random()*100) ;
+  const wheel = document.getElementById('wheel');
+  console.log(radius);
+  console.log(wheel)
+
+  const handleButton = (radius) => {
+
+    wheel.style.rotate(radius)
+  }
+  
+
   return (
     <div className="App">
 
         <div className='spin'>
-          
+
           <h1>Spin the wheel </h1>
+
+          <span id="wheel"> </span>
+
+          <button className="spinButton"  onClick={handleButton}> Spin the wheel</button>
       
          </div>
     </div>
