d5b81cd4d38c9b71d7ed04c32d5f88e5b4d9af37
unknown <marcinoles87@gmail.com>
2022-11-26 18:15:02 +0100

    functionality

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
     wheel.classList.add('rotate')
    } else {
     console.log("przestaje dzialac")
+    wheel.classList.remove('rotate');
    }
    
     this.setState(()=>({
